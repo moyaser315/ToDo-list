307dad2e57b8b6c1325ba31f4442cb5fbb388e1a
moyaser315 <88896344+moyaser315@users.noreply.github.com>
2022-08-07 08:51:56 +0200

    seperating files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const day=require(__dirname+"/day.js");
 app.set('view engine', 'ejs');
 app.listen(3000, function() {
   console.log("service has started on port 3000");
@@ -9,44 +10,9 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
-let today = new Date();
-let dayNum = today.getDay();
-let day = "";
 let items=[];
 let workList=[]
-options = {
-  day:"2-digit" ,
-  month:"long" ,
-  year:"numeric"
-}
-today = today.toLocaleDateString("en-US", options);
-
-switch (dayNum) {
-  case 0:
-    day = "sunday";
-    break;
-  case 1:
-    day = "monday";
-    break;
-  case 2:
-    day = "tuesday";
-    break;
-  case 3:
-    day = "wednsday";
-    break;
-  case 4:
-    day = "thursday";
-    break;
-  case 5:
-    day = "friday";
-    break;
-  case 6:
-    day = "saturday";
-    break;
-  default:
-    console.log("the num entered switch is :" + dayNum);
-}
-today = day+" "+today;
+let today=day();
 
 app.get("/", function(req, res) {
   let loops=items.length;
---
+++ day.js
@@ -0,0 +1,40 @@
+module.exports=getDay;
+function getDay(){
+let today = new Date();
+let dayNum = today.getDay();
+let day = "";
+options = {
+  day:"2-digit" ,
+  month:"long" ,
+  year:"numeric"
+}
+today = today.toLocaleDateString("en-US", options);
+
+switch (dayNum) {
+  case 0:
+    day = "sunday";
+    break;
+  case 1:
+    day = "monday";
+    break;
+  case 2:
+    day = "tuesday";
+    break;
+  case 3:
+    day = "wednsday";
+    break;
+  case 4:
+    day = "thursday";
+    break;
+  case 5:
+    day = "friday";
+    break;
+  case 6:
+    day = "saturday";
+    break;
+  default:
+    console.log("the num entered switch is :" + dayNum);
+}
+today = day+" "+today;
+return today;
+}
