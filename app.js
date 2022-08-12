7fb790dffaa33405476e1b7d6717c16f0df60945
moyaser315 <88896344+moyaser315@users.noreply.github.com>
2022-08-12 11:41:33 +0200

    working version before creating multi pages

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+//          required npms :
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const day=require(__dirname+"/day.js");
+const day = require(__dirname + "/day.js"); //local file
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/todolist');
+//                        mongoose scheme
+const itemsScheme = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  }
+});
+const Item = mongoose.model("itemList", itemsScheme);
+const WorkItem = mongoose.model("workList", itemsScheme);
+//                    to use ejs
 app.set('view engine', 'ejs');
+//                    to start server
 app.listen(3000, function() {
   console.log("service has started on port 3000");
 });
@@ -10,38 +24,79 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
-let items=[];
-let workList=[]
-let today=day.getDay();
+
+
+
+
+//////////////////                            start logic here
+let items = [];
+let workList = []
+let today = day.getDay();
+/////////////       always displayed
+const work = new Item({
+  name: "work"
+});
+const play = new Item({
+  name: "play"
+});
+const study = new Item({
+  name: "study"
+});
+Item.find({}, function(err, docs) {
+  if (err) {
+    console.log(err);
+  } else {
+    docs.forEach(function(log) {
+      items.push(log.name);
+    });
+  }
+});
+
 
 app.get("/", function(req, res) {
-  let loops=items.length;
+  let loops = items.length;
   res.render("lists", {
-    kindOfDay: today ,
+    kindOfDay: today,
     newItem: items,
-    loops:loops
+    loops: loops
   });
 })
 app.post("/", function(req, res) {
-  let newItem = req.body.new
-  if(req.body.button=="work"){
-    workList.push(newItem);
+  let newItem = new Item({
+    name: req.body.new
+  });
+  let newWItem = new WorkItem({
+    name: req.body.new
+  });
+  if (req.body.button == "work") {
+    newWItem.save();
+    workList.push(newWItem.name);
     res.redirect("/work");
-  }else{
-    items.push(newItem);
+  } else {
+    newItem.save();
+    items.push(newItem.name);
     res.redirect("/");
   }
-
 })
-app.get("/work",function(req,res){
-  let loops=workList.length;
-  res.render("lists",{kindOfDay:"work list", newItem: workList,
-  loops:loops})
+app.get("/work", function(req, res) {
+  let loops = workList.length;
+  res.render("lists", {
+    kindOfDay: "work list",
+    newItem: workList,
+    loops: loops
+  })
 })
-app.post("/work",function(req,res){
+app.post("/work", function(req, res) {
   workList.push(req.body.new);
   res.redirect("/work");
 })
-app.get("/about",function(req,res){
+app.get("/about", function(req, res) {
   res.render("about");
+})
+app.post("/delete",function(req,res){
+  let x =req.body.check;
+  let index=items.indexOf(x);
+  items.splice(index,1);
+  Item.deleteOne({name:x},function(err){console.log(err);})
+  res.redirect("/");
 })
